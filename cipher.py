233e7ea77cb38bf345b14ddfbc85923f7b2613ed
Filip <5021567@gmail.com>
2018-12-07 19:56:13 +0100

    Initial commit

---
+++ cipher.py
@@ -0,0 +1,106 @@
+from PIL import Image
+
+import random
+import sys
+
+path = "C:\\Users\\50215\\Desktop"
+
+image = Image.open(path + "\\in.bmp")
+image = image.convert('1')
+
+width, height = image.size
+outfile1 = Image.new("1", (width*2, height*2), "white")
+
+outfile2 = Image.new("1", (width*2, height*2), "white")
+
+for x in range(0, width):
+    for y in range(0, height):
+        sourcepixel = image.getpixel((x, y))
+        assert sourcepixel in (0, 255)
+        coinflip = random.random()
+        if sourcepixel == 0:
+            if coinflip < .25:
+                outfile1.putpixel((x * 2, y * 2), 255)
+                outfile1.putpixel((x * 2 + 1, y * 2), 0)
+                outfile1.putpixel((x * 2, y * 2 + 1), 0)
+                outfile1.putpixel((x * 2 + 1, y * 2 + 1), 255)
+
+                outfile2.putpixel((x * 2, y * 2), 0)
+                outfile2.putpixel((x * 2 + 1, y * 2), 255)
+                outfile2.putpixel((x * 2, y * 2 + 1), 255)
+                outfile2.putpixel((x * 2 + 1, y * 2 + 1), 0)
+            elif (.25 <= coinflip) and (coinflip < .5):
+                outfile1.putpixel((x * 2, y * 2), 0)
+                outfile1.putpixel((x * 2 + 1, y * 2), 0)
+                outfile1.putpixel((x * 2, y * 2 + 1), 255)
+                outfile1.putpixel((x * 2 + 1, y * 2 + 1), 255)
+
+                outfile2.putpixel((x * 2, y * 2), 255)
+                outfile2.putpixel((x * 2 + 1, y * 2), 255)
+                outfile2.putpixel((x * 2, y * 2 + 1), 0)
+                outfile2.putpixel((x * 2 + 1, y * 2 + 1), 0)
+            elif (.5 <= coinflip) and (coinflip < .75):
+                outfile1.putpixel((x * 2, y * 2), 255)
+                outfile1.putpixel((x * 2 + 1, y * 2), 255)
+                outfile1.putpixel((x * 2, y * 2 + 1), 0)
+                outfile1.putpixel((x * 2 + 1, y * 2 + 1), 0)
+
+                outfile2.putpixel((x * 2, y * 2), 0)
+                outfile2.putpixel((x * 2 + 1, y * 2), 0)
+                outfile2.putpixel((x * 2, y * 2 + 1), 255)
+                outfile2.putpixel((x * 2 + 1, y * 2 + 1), 255)
+            else:
+                outfile1.putpixel((x * 2, y * 2), 255)
+                outfile1.putpixel((x * 2 + 1, y * 2), 0)
+                outfile1.putpixel((x * 2, y * 2 + 1), 255)
+                outfile1.putpixel((x * 2 + 1, y * 2 + 1), 0)
+
+                outfile2.putpixel((x * 2, y * 2), 0)
+                outfile2.putpixel((x * 2 + 1, y * 2), 255)
+                outfile2.putpixel((x * 2, y * 2 + 1), 0)
+                outfile2.putpixel((x * 2 + 1, y * 2 + 1), 255)
+
+        elif sourcepixel == 255:
+            if coinflip < .25:
+                outfile1.putpixel((x * 2, y * 2), 255)
+                outfile1.putpixel((x * 2 + 1, y * 2), 0)
+                outfile1.putpixel((x * 2, y * 2 + 1), 0)
+                outfile1.putpixel((x * 2 + 1, y * 2 + 1), 255)
+
+                outfile2.putpixel((x * 2, y * 2), 255)
+                outfile2.putpixel((x * 2 + 1, y * 2), 0)
+                outfile2.putpixel((x * 2, y * 2 + 1), 0)
+                outfile2.putpixel((x * 2 + 1, y * 2 + 1), 255)
+            elif (.25 <= coinflip) and (coinflip < .5):
+                outfile1.putpixel((x * 2, y * 2), 0)
+                outfile1.putpixel((x * 2 + 1, y * 2), 0)
+                outfile1.putpixel((x * 2, y * 2 + 1), 255)
+                outfile1.putpixel((x * 2 + 1, y * 2 + 1), 255)
+
+                outfile2.putpixel((x * 2, y * 2), 0)
+                outfile2.putpixel((x * 2 + 1, y * 2), 0)
+                outfile2.putpixel((x * 2, y * 2 + 1), 255)
+                outfile2.putpixel((x * 2 + 1, y * 2 + 1), 255)
+            elif (.5 <= coinflip) and (coinflip < .75):
+                outfile1.putpixel((x * 2, y * 2), 255)
+                outfile1.putpixel((x * 2 + 1, y * 2), 255)
+                outfile1.putpixel((x * 2, y * 2 + 1), 0)
+                outfile1.putpixel((x * 2 + 1, y * 2 + 1), 0)
+
+                outfile2.putpixel((x * 2, y * 2), 255)
+                outfile2.putpixel((x * 2 + 1, y * 2), 255)
+                outfile2.putpixel((x * 2, y * 2 + 1), 0)
+                outfile2.putpixel((x * 2 + 1, y * 2 + 1), 0)
+            else:
+                outfile1.putpixel((x * 2, y * 2), 255)
+                outfile1.putpixel((x * 2 + 1, y * 2), 0)
+                outfile1.putpixel((x * 2, y * 2 + 1), 255)
+                outfile1.putpixel((x * 2 + 1, y * 2 + 1), 0)
+
+                outfile2.putpixel((x * 2, y * 2), 255)
+                outfile2.putpixel((x * 2 + 1, y * 2), 0)
+                outfile2.putpixel((x * 2, y * 2 + 1), 255)
+                outfile2.putpixel((x * 2 + 1, y * 2 + 1), 0)
+
+outfile1.save(path + '\\out1.bmp')
+outfile2.save(path + '\\out2.bmp')
